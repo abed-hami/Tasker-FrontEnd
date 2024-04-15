5455537368253ad89685e3da485a2596321e8ae6
abedhamiyeh <abedhamye28@gmail.com>
2024-04-15 22:46:12 +0300

    kanban board

--- src/app/services/kanban.service.ts
+++ src/app/services/kanban.service.ts
@@ -11,4 +11,16 @@ export class KanbanService {
   getList(id:any,status:any){
     return this.http.get("https://localhost:7183/api/ToDo/GetByStatus/"+id+"/"+status)
   }
+
+  updateStatus(id:any, status:any){
+    return this.http.put("https://localhost:7183/api/ToDo/updateStatus/"+id+"/"+status,null)
+  }
+
+  addToDo(object:any){
+    return this.http.post("https://localhost:7183/api/ToDo",object)
+  }
+
+  delete(id:any){
+    return this.http.delete("https://localhost:7183/api/ToDo/"+id)
+  }
 }
